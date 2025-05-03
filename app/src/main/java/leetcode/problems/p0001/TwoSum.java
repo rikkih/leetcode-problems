edef6a4ddc6a584c42c4f365bd13f87654986f32
Hoderick <rikkih@protonmail.com>
2025-05-03 17:18:56 +0100

    finish up two sum

--- app/src/main/java/leetcode/problems/p0001/TwoSum.java
+++ app/src/main/java/leetcode/problems/p0001/TwoSum.java
@@ -1,5 +1,8 @@
 package leetcode.problems.p0001;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class TwoSum {
     int[] solveSortedArray(int[] nums, int target) {
         int left = 0;
@@ -19,10 +22,27 @@ public class TwoSum {
         return new int[] { -1, -1 };
     }
 
+    int[] solveUnsortedArray(int[] nums, int target) {
+        Map<Integer, Integer> complements = new HashMap<>();
+
+        for (int i = 0; i < nums.length; i++) {
+
+            int complement = target - nums[i];
+
+            if (complements.containsKey(complement)) {
+                return new int[] { complements.get(complement), i };
+            }
+
+            complements.put(nums[i], i);
+        }
+
+        return new int[] { -1, -1 };
+    }
+
     public static void main(String[] args) {
         TwoSum twoSum = new TwoSum();
 
-        var answer = twoSum.solve(new int[] { 2, 7, 11, 15 }, 9);
+        var answer = twoSum.solveUnsortedArray(new int[] { 2, 7, 11, 15 }, 9);
 
         System.out.println("Answer: [" + answer[0] + answer[1] + "]");
     }
