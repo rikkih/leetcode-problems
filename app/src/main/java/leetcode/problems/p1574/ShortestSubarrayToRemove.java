f32e8d2fa92c5a29be43e90ddd0db07a262599d6
Hoderick <rikkih@protonmail.com>
2025-05-06 19:00:37 +0100

    working solution for shortest subarray to remove

--- app/src/main/java/leetcode/problems/p1574/ShortestSubarrayToRemove.java
+++ app/src/main/java/leetcode/problems/p1574/ShortestSubarrayToRemove.java
@@ -2,37 +2,48 @@ package leetcode.problems.p1574;
 
 class ShortestSubarrayToRemove {
     int solve(int[] arr) {
-        if (arr.length < 2) {
-            return arr[0];
-        }
+        int n = arr.length;
 
-        int shortestLength = 0;
-        int leftPointer = 0;
-        int rightPointer = arr.length - 1;
-
-        while (leftPointer < arr.length - 1 && arr[leftPointer] <= arr[leftPointer + 1]) {
-            leftPointer++;
-        }
-
-        while (rightPointer > 0 && arr[rightPointer] >= arr[rightPointer - 1]) {
-            rightPointer--;
-        }
-
-        if (leftPointer >= rightPointer) {
-            return shortestLength;
-        }
-
+        // 1. Find the leftmost non-decreasing subarray.
         int left = 0;
-        int right = leftPointer;
+        while (left + 1 < n && arr[left] <= arr[left + 1]) {
+            left++;
+        }
 
-        while (left <= right) {
-            int mid = left + (right - left) / 2;
+        if (left == n - 1) {
+            return 0;
+        }
 
-            if (arr[mid] <= arr[rightPointer]) {
-                shortestLength = (rightPointer - 1) - mid;
-                left = mid + 1;
-            } else {
-                right = mid - 1;
+        // 2. Find the rightmost non-decreasing subarray.
+        int right = n - 1;
+        while (right > 0 && arr[right] >= arr[right - 1]) {
+            right--;
+        }
+
+        // 3. Set the first possible solution to shortest sub array length.
+        int shortestLength = Math.min(n - left - 1, right);
+
+        // 4. Use Binary Search to try and merge [0,...,i] with [j,...,n-1] for each i.
+        // The loop ends when l == r, and l is the first index where arr[l] >= arr[i],
+        // or l == n if no such index exists.
+        for (int i = 0; i <= left; i++) {
+
+            int l = right, r = n;
+            while (l < r) {
+                int mid = (l + r) / 2;
+
+                if (arr[i] <= arr[mid]) {
+                    r = mid; // Potential candidate found — search left to find first such index
+                } else {
+                    l = mid + 1; // Too small, so search exclusive right hand side.
+                }
+            }
+
+            // Check if the merge is the minimum
+            // l came from binary search. If we didn't find a valid index, it "falls off the
+            // end" and equals n.
+            if (l < n) {
+                shortestLength = Math.min(shortestLength, l - i - 1);
             }
         }
         return shortestLength;
@@ -41,7 +52,7 @@ class ShortestSubarrayToRemove {
     public static void main(String[] args) {
         ShortestSubarrayToRemove shortest = new ShortestSubarrayToRemove();
 
-        var answer = shortest.solve(new int[] { 5, 4, 3, 2, 1 });
+        var answer = shortest.solve(new int[] { 1, 2, 3 });
         System.out.println(answer);
     }
 }
