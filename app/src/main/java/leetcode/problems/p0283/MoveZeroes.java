b23466b0fe95f83556cfba37f6ba578b2c99df82
Hoderick <rikkih@protonmail.com>
2025-05-08 07:59:03 +0100

    add one solution for move zeroes

---
+++ app/src/main/java/leetcode/problems/p0283/MoveZeroes.java
@@ -0,0 +1,31 @@
+package leetcode.problems.p0283;
+
+import java.util.Arrays;
+
+class MoveZeroes {
+    void solveWithSwap(int[] nums) {
+        int lastZero = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != 0) {
+                int temp = nums[lastZero];
+                nums[lastZero] = nums[i];
+                nums[i] = temp;
+                lastZero++;
+            }
+        }
+    }
+
+    void solveWithForwardPass(int[] nums) {
+        // TODO
+    }
+
+    public static void main(String[] args) {
+        MoveZeroes moveZeroes = new MoveZeroes();
+
+        var testCase = new int[] { 1, 0, 1, 0, 3, 12 };
+
+        moveZeroes.solve(testCase);
+        System.out.println(Arrays.toString(testCase));
+    }
+}
