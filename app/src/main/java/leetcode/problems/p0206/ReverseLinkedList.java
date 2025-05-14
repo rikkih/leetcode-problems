96fc6aefe9c9901c66aa491a1032ffbb0ccbf774
Hoderick <rikkih@protonmail.com>
2025-05-14 18:08:05 +0100

    Reverse Linked List

---
+++ app/src/main/java/leetcode/problems/p0206/ReverseLinkedList.java
@@ -0,0 +1,36 @@
+package leetcode.problems.p0206;
+
+import leetcode.util.ListNode;
+
+import java.util.logging.Logger;
+
+class ReverseLinkedList {
+
+    private static final Logger logger = Logger.getLogger(ReverseLinkedList.class.getName());
+
+    ListNode reverseList(ListNode head) {
+        ListNode prev = null;
+        ListNode current = head;
+
+        while (current != null) {
+            ListNode next = current.next; // Get the next node and assign it temp.
+            current.next = prev;          // reverse pointer of current (1 -> null) (2 -> 1) ... (5 -> 4)
+            prev = current;               // For the next iteration, make prev the current
+            current = next;               // Make current the next node. Eventually null.
+        }
+        return prev;
+    }
+
+    public static void main(String[] args) {
+        ReverseLinkedList reverseLinkedList = new ReverseLinkedList();
+
+        ListNode node5 = new ListNode(5);
+        ListNode node4 = new ListNode(4, node5);
+        ListNode node3 = new ListNode(3, node4);
+        ListNode node2 = new ListNode(2, node3);
+        ListNode node1 = new ListNode(1, node2);
+
+        ListNode reversed = reverseLinkedList.reverseList(node1);
+        logger.info("Answer: " + reversed);
+    }
+}
