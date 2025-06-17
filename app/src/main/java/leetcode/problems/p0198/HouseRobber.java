1febaecd0960fa7b4d37d73ce37237acefd6ed7a
Hoderick <rikkih@protonmail.com>
2025-06-17 23:09:42 +0100

    house robber solution

--- app/src/main/java/leetcode/problems/p0198/HouseRobber.java
+++ app/src/main/java/leetcode/problems/p0198/HouseRobber.java
@@ -5,9 +5,24 @@ import java.util.Arrays;
 public class HouseRobber {
 
     int rob(int[] nums) {
-        int[] memo = new int[nums.length];
-        Arrays.fill(memo, -1);
-        return 0;
+        /**
+         * If we skip a house, we take the money from the house before.
+         * If we rob from a house, we can't take from the previous adjacent house, but the one before.
+         * At each house we therefore have a choice.
+         * Skip, and don't take from this house, take i-1th. Rob from this house, and take the max from i-2th
+         */
+        int n = nums.length;
+
+        int first = nums[0];
+        int second = Math.max(nums[0], nums[1]);
+
+        for (int i = 2; i < n; i++) {
+            int current = Math.max(second, nums[i] + first);
+            first = second;
+            second = current;
+        }
+
+        return second;
     }
 
     public static void main(String[] args) {
