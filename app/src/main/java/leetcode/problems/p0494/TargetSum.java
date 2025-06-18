a8a7e31e91db50992a2e717b17f9a8be132bf80d
Hoderick <rikkih@protonmail.com>
2025-06-18 16:52:59 +0100

    Target Sum with optimal Bottom Up iterative approach

--- app/src/main/java/leetcode/problems/p0494/TargetSum.java
+++ app/src/main/java/leetcode/problems/p0494/TargetSum.java
@@ -31,9 +31,46 @@ public class TargetSum {
         return result;
     }
 
+    /**
+     * We want to try and map this problem on to the subset sum problem.
+     *
+     * One thing we can see is that 0 <= nums[i] <= 1000, so all elements are positive.
+     * Each element will be assigned a plus or negative symbol. That is,
+     *
+     * sum(+) - sum(-) = target; we also know that
+     * sum(+) + sum(-) = totalSum;
+     * ==>
+     * 2 * sum(+) = totalSum + target;
+     * ==>
+     * sum(+) = (target + totalSum) / 2;
+     *
+     * Thus, we need to find a subset whose sum is equal to (target + totalSum) / 2
+     * @param nums
+     * @return
+     */
+    int targetSumIterative(int[] nums, int target) {
+        int total = Arrays.stream(nums).sum();
+        if ((target + total) % 2 != 0 || Math.abs(target) > total) return 0;
+
+        int subsetSum = (target + total) / 2;
+
+        // dp[i] = the number of ways to pick elements from nums[] such that they sum to i.
+        // So dp[0] = 1 initially — there's 1 way to sum to zero: pick nothing.
+        // "If we already know how many ways to make sum i - num, then we can make sum i by adding num to those combinations."
+        int[] dp = new int[subsetSum + 1];
+        dp[0] = 1;
+
+        for (int num : nums) {
+            for (int i = subsetSum; i >= num; i--) {
+                dp[i] += dp[i - num];
+            }
+        }
+        return dp[subsetSum];
+    }
+
     public static void main(String[] args) {
         TargetSum targetSum = new TargetSum();
-        var result = targetSum.bruteForce(new int[]{1, 2, 3}, 0);
+        var result = targetSum.targetSumIterative(new int[]{1, 2, 3}, 0);
         System.out.println(result);
     }
 }
